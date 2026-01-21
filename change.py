583fcfb1a5caaa607c14e79d15ce256965929a67
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-21 13:53:36 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,2 +1,4 @@
+before that we need to fix nemotron... we need to add try-catch at suitable places to see why the newly written typescript code is not able to connect
+ 
 I am seeing this error: RuntimeError: Unexpected ASGI message 'websocket.send', after sending 'websocket.close' or response already completed
 
