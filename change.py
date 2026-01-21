eef4b61eae8eb285442f820cd5dc290e51337e06
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-21 12:08:40 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,6 +1,4 @@
-(asr_env) PS C:\Users\re_nikitav\Desktop\cx_asr_realtime\scripts> python .\ws_client.py --mic --url ws://127.0.0.1:8003/ws/asr
-[INFO] Connected to ws://127.0.0.1:8003/ws/asr
-🎤 Speak freely. Pause to end sentences. Ctrl+C to exit.
-[PARTIAL] Hello, this is Mike Testing, I'm testing ASR Nemoton real time if it is emitting the matrix or not
-[FINAL] Hello, this is Mike Testing, I'm testing ASR Nemoton real time if it is emitting the matrix or not
-[SERVER_METRICS] reason=pause ttft_ms=517 ttf_ms=7635 audio_ms=7600 rtf=0.252732925674584 chunks=26 preproc_ms=466 infer_ms=1920 flush_ms=40
+ # model = "parakeet-1.1b-en-US-asr-offline-silero-vad-asr-bls-ensemble",
+            # model = "parakeet-1.1b-en-US-asr-offline-asr-bls-ensemble"
+            # model = "canary-0.6b-turbo-multi-asr-offline-asr-bls-ensemble",
+            # model = "conformer-en-US-asr-offline-asr-bls-ensemble"
